b0d0a957fcb427e3bbb37650fb671d5ab4962d90
Danico19827 <117318930+Danico19827@users.noreply.github.com>
2023-04-26 22:31:16 -0300

    nuevo

--- src/main/java/com/example/backend/WebConfig.java
+++ src/main/java/com/example/backend/WebConfig.java
@@ -1,28 +1,31 @@
 
 package com.example.backend;
-    
+/**    
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  
-/**
+
 
  * Clase que habilita CORS
 
  * @author YOProgramo
 
- */
+
 
 @EnableWebMvc
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-
+*/
+public class WebConfig /*implements WebMvcConfigurer*/ {
+/*
     @Override
 
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**");
     }
+*/
 }
     
+ 
